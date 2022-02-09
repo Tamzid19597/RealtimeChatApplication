5597903a3af9c321d817853f5a732053983bac8f
Tamzid19597 <Tamzid19597@users.noreply.github.com>
2022-02-10 01:22:05 +0600

    version-01

---
+++ springboot-realtimechat/src/main/java/com/tamzid/realtimechat/SpringBootRealtimeChatApplication.java
@@ -0,0 +1,13 @@
+package com.tamzid.realtimechat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRealtimeChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootRealtimeChatApplication.class, args);
+    }
+
+}
